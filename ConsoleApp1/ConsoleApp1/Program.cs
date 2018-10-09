f0b6c42d5397297a00267f19d65ffc9e6147071f
Ynon07 <43990146+Ynon07@users.noreply.github.com>
2018-10-09 15:11:51 +0300

    Ynon Hatotach

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Git Hub works");
+            Console.WriteLine("Git Hub works Ynon Hatotach");
         }
     }
 }
